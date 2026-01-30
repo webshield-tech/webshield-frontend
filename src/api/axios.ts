2fc007938a6980ab460c3d5b59ce57793013bd10
Husnain <crazyitx3@gmail.com>
2026-01-30 22:11:09 +0500

    replaced backend URI

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL || 
-  "https://webshield-backend-production-1871.up.railway.app";
+  "https://webshield-backend-e051bc08c935.herokuapp.com/";
 
 console.log('API Base URL:', BASE_URL);
 
